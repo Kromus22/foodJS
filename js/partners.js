0d48baa2923ca1650dcd313b497231d68569f5fe
Kromus22 <kromus2@gmail.com>
2021-11-08 21:19:14 +0500

    ..

--- js/partners.js
+++ js/partners.js
@@ -6,7 +6,7 @@ const partners = () => {
             const { image, kitchen, name, price, products, stars, time_of_delivery } = item
             const a = document.createElement('a')
 
-            a.setAttribute('href', '/restaurant.html')
+            a.setAttribute('href', './restaurant.html')
             a.classList.add('card')
             a.classList.add('card-restaurant')
             a.dataset.products = products
@@ -33,7 +33,7 @@ const partners = () => {
 
                 localStorage.setItem('restaurant', JSON.stringify(item))
 
-                window.location.href = '/restaurant.html'
+                window.location.href = './restaurant.html'
             })
 
             cardsRestaurants.append(a)
